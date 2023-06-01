1af0670157516f217d447aa43485a8e4a620b7ae
PrashantGyawali <49340437+PrashantGyawali@users.noreply.github.com>
2023-06-01 19:27:45 +0545

    Working MVP

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rand::thread_rng;
 use rand::Rng;
-use crossterm::style::{Attribute, Color, Stylize};
+use crossterm::style::{ Color, Attribute, Stylize};
 use std::io;
 
 fn main() {
@@ -10,51 +10,79 @@ fn main() {
 
     let random_str=random.to_string();
     let random_chars: Vec<char> = random_str.chars().collect();
-    println!("{}",random);
 
 loop{
     let mut guess_str = String::new();
+    print!("\x1b[0m");
     io::stdin().read_line(&mut guess_str).expect("Failed to read line");
-    let guess_chars: Vec<char> = guess_str.chars().collect();
     let guess: Result<u32, _> = guess_str.trim().parse();
 
 
-    if let Ok(num) = guess {
-        loopfn(num,guess_chars,guess_str,random,random_str.clone(),random_chars.clone());
-    } else {
+    if let Ok(num) = guess 
+    {
+       let i= loopfn(num,random,random_chars.clone());
+        if i
+        { continue;}
+        else {break;}
+        
+    } 
+    else {
         println!("Give proper number");
     }
 
 
-fn loopfn(mut my_int:u32,my_int_chars:Vec<char>,my_int_str:String,r:i32,r_str: String,r_chars:Vec<char>){
+fn loopfn(mut my_int:u32,r:i32,r_chars:Vec<char>)->bool{
 
-    loop {
+    while my_int>99999||my_int<10000 {
         if my_int > 99999 {
             my_int = my_int / 10;
-        } else if my_int < 1000 {
+        } 
+        else {
             my_int = my_int * 10;
-        } else {
-            break;
-        }
+        } 
     }
 
+    let my_int_str=my_int.to_string();
+    let my_int_chars: Vec<char>=my_int_str.chars().collect();
 
-
+    let mut k="".to_string();
+    let mut close=0;
 
     for n in 0..my_int_chars.len()
     {
         if my_int_chars.get(n) == r_chars.get(n)
         {
-            let styledint = my_int_chars[n].with(Color::Green).attribute(Attribute::Bold);
-            print!("{}",styledint);
+            k=format!("{}\x1b[32m{a}\x1b[0m",k,a=my_int_chars[n]);
+            close+=3;
         }
         else if  r_chars.iter().any(|x| *x==my_int_chars[n]){
-            let styledint = my_int_chars[n].with(Color::Yellow).attribute(Attribute::Bold);
-            print!("{}",styledint);   
+            k=format!("{}\x1b[33m{a}\x1b[0m",k,a=my_int_chars[n]);       
+            close+=1;
         }
         else{
-            print!("{}",my_int_chars[n]);   
+            k=format!("{}\x1b[31m{a}\x1b[0m",k,a=my_int_chars[n]);       
         }
+        }
+    
+    println!("{}",k);
+
+    if r.to_string()==my_int.to_string()
+    {
+        println!("You guessed it correct. Great job");
+        io::stdin().read_line(&mut String::new()).unwrap();
+        return false;
+    }
+    else if close==7{
+        println!("So close, Try again:");
+        return true;
+    }
+    else if close>9{
+        println!("Nearly there, Try again:");
+        return true;
+    }
+    else{
+        println!("Try again:");
+        return true;
     }
 
 
@@ -67,9 +95,13 @@ fn loopfn(mut my_int:u32,my_int_chars:Vec<char>,my_int_str:String,r:i32,r_str: S
 
 fn welcome() {
     print!("\x1B[2J\x1B[1;1H");
+    let styled = "Welcome to guessing game"
+    .with(Color::Blue)
+    .on(Color::Black)
+    .attribute(Attribute::Bold);
 
-    println!("Welcome to guessing game!");
-    println!("I have a 4 digit number for you. Can you guess what it is?");
+    println!("{}", styled);
+    println!("I have a 5 digit number for you. Can you guess what it is?");
 }
 
 fn randomnum()->i32
